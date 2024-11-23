6546e0f08f9b2a9fb2f9b8dbeddf2c2e7245828f
Dibyajyoti <dibyajyoti@Dibyajyotis-MacBook-Pro.local>
2024-11-24 00:55:14 +0530

    added create contact

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ const bodyparser = require('body-parser')
 app.use(bodyparser.json());
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Welcome home');
 })
-app.post('/identify', async(req,res)=>{
-    const body = req.body;
-    console.log(body);
-    res.status(200).json({'body': body})
+
+app.post('/identify', async (req, res) => {
+    const { email, phoneNumber } = req.body;
+    if (!email && !phoneNumber) {
+        return res.status(400).json({ error: "Email or Phone number is requierd" })
+    }
+    const contactToBeAdded = await prisma.contact.create({
+        data: {
+            email: email,
+            phoneNumber: phoneNumber
+        }
+    })
+    console.log(contactToBeAdded);
+    res.status(200).json({
+        contact: {
+            primaryContatctId: contactToBeAdded.id,
+            emails: [contactToBeAdded.email],
+            phoneNumbers: [contactToBeAdded.phoneNumber],
+            secondaryContactIds: []
+        }
+    })
+
 })
-app.listen(PORT, ()=>{
+
+app.listen(PORT, () => {
     console.log('Server running on port 4000')
 })
