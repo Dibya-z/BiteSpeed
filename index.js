dea2036700481c3d680fc8ec011080427498b5c7
Dibyajyoti <dibyajyoti@Dibyajyotis-MacBook-Pro.local>
2024-11-24 06:52:30 +0530

    added logic for how secondary contact is created if same number or email is passed using 1parent id

--- index.js
+++ index.js
@@ -14,24 +14,110 @@ app.get('/', (req, res) => {
 
 app.post('/identify', async (req, res) => {
     const { email, phoneNumber } = req.body;
+    console.log(email,phoneNumber)
     if (!email && !phoneNumber) {
         return res.status(400).json({ error: "Email or Phone number is requierd" })
     }
-    const contactToBeAdded = await prisma.contact.create({
-        data: {
-            email: email,
-            phoneNumber: phoneNumber
+    const relatedContacts = await prisma.contact.findMany({
+        where: {
+            OR: [{ email},{phoneNumber}]
         }
-    })
-    console.log(contactToBeAdded);
-    res.status(200).json({
-        contact: {
-            primaryContatctId: contactToBeAdded.id,
-            emails: [contactToBeAdded.email],
-            phoneNumbers: [contactToBeAdded.phoneNumber],
-            secondaryContactIds: []
+    });
+    console.log(relatedContacts);
+    if (relatedContacts.length === 0) {
+        const contactToBeAdded = await prisma.contact.create({
+            data: {
+                email: email,
+                phoneNumber: phoneNumber
+            }
+        })
+        console.log(contactToBeAdded);
+        return res.status(200).json({
+            contact: {
+                primaryContatctId: contactToBeAdded.id,
+                emails: [contactToBeAdded.email],
+                phoneNumbers: [contactToBeAdded.phoneNumber],
+                secondaryContactIds: []
+            }
+        })
+
+    }
+    else {
+        const mySet = new Set();
+
+        for (let i = 0; i < relatedContacts.length; i++) {     //adding all the ids of parent to set to check
+            if (relatedContacts[i].linkPrecedence === "secondary") {
+                mySet.add(relatedContacts[i].linkedId);
+            }
+            else {
+                mySet.add(relatedContacts[i].id)
+            }
         }
-    })
+        const arr = [...mySet]
+        if (mySet.size === 1) {   //if 1 parent id that means case-2
+            if (email && phoneNumber) {
+                const contact = await prisma.contact.create({
+                    data: {
+                        email: email,
+                        phoneNumber: phoneNumber,
+                        linkedId:  arr[0],
+                        linkPrecedence: "secondary"
+                    }
+                })
+
+            }
+            const primaryMail = await prisma.contact.findUnique({
+                where : {id : arr[0]},
+                select : {
+                    email : true,
+                }
+            })
+            const secondaryEmails = await prisma.contact.findMany({
+                where : {linkedId : arr[0]},
+                select : {
+                    email : true,
+                }
+            })
+            const primaryNumber = await prisma.contact.findUnique({
+                where : {id : arr[0]},
+                select : {
+                    phoneNumber : true,
+                }
+            })
+            const secondaryNumber = await prisma.contact.findMany({
+                where : {linkedId : arr[0]},
+                select : {
+                    phoneNumber : true,
+                }
+            })
+            const secondaryIds = await prisma.contact.findMany({
+                where : {linkedId : arr[0]},
+                select : {
+                    id : true
+                }
+            })
+            const mail = [primaryMail.email, ...secondaryEmails.map((item) => item.email)]
+            const set = new Set(mail);
+            const mails = [...set]
+            const numbers = [primaryNumber.phoneNumber, ...secondaryNumber.map((item) => item.phoneNumber)]
+            const newset = new Set(numbers);
+            const nums = [...newset]
+            const secondaryContactIds = secondaryIds.map((item) => item.id);
+            return res.status(200).json({
+                contact: {
+                    primaryContatctId: arr[0],
+                    emails: mails,
+                    phoneNumbers: nums,
+                    secondaryContactIds: secondaryContactIds
+                }
+            })
+        }
+        else if (mySet.size === 2) {    //  if 2 parents id that means case-3
+            
+        }
+
+    }
+
 
 })
 
